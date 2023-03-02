1c7c6df1134c78850fa3b24c5cb1ca51f4ae618b
Annie-12 <46844722+Annie-12@users.noreply.github.com>
2023-03-02 15:06:52 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ class Test{
   public static void main(String[] args){
     string password = "Abc";
     string key = "oop";
+    string pat = "ghp_ApH07wTahlmdfDL4PXmRCVvu7l3Y8b3YsX4K";
     System.out.println(password);
   }
 }
